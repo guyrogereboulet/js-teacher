f72c4a384e1a5546622369bf49c9a4280ca03dff
Guy Roger Eboulet <eboulet87@gmail.com>
2020-11-03 16:36:58 +0100

    Commit

--- 32-esercitazioni-2/lib.js
+++ 32-esercitazioni-2/lib.js
@@ -1,13 +1,24 @@
 // 1) Scrivere una funzione che accetta una stringa e restituisce la parola più lunga all'interno della stringa
 
 function maxString(str) {
+    // Trasformiamo un la stringa "str" passata nella funzione in un array
+    let parole = str.split(" ");
+    //Definiamo una variabile che conterrà la parola più lunga con posizione 0 nell'array stringa
+    let max = parole[0];
 
-    let stringa = str.split(" ");
-    let max = stringa[0];
+    for (let i = 0; i< parole.length; i++) {
 
-    for (let i = 0; i< stringa.length; i++) {
+        /*Nel caso in cui la stringa che si trova nell'array "parole" è maggiore della lunghezza della stringa della variabile "max" allora assegnamo a "max" "parole" altrimenti riassegnamo max
+        */
+        max = parole[i].length > max.length ? parole[i] : max;
+
+        //Operatore ternario
+
+        /*
+
+        condizione ? espressione1 : espressione2
         
-      max = stringa[i].length > max.length ? stringa[i] : max;
+        */
         
     }
 
@@ -22,14 +33,25 @@ let result = maxString(frase);
 console.log(result);
 
 
-// 2) scrovere una funzione ricorsiva che, dato un valore x e un valore n, esegue la potenza di x alla n
+// 2) scrivere una funzione ricorsiva che, dato un valore x e un valore n, esegue la potenza di x alla n
 
+function esponente (x, n) {
 
+    if (x == 1) {
 
+        return 1;
 
+    }
 
+    return  n == 1 ? x : x * esponente(x, n -1);
+ };
+
+ let ris = esponente(7,2);
+
+ console.log(ris);
 
 // 3) Scrivere una funzione che ritorna una stringa casuale di n caratteri
+
 
 
 
