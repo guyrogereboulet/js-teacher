89bf5ccdc641b096a86c4485fa978125d122e911
Guy Roger Eboulet <eboulet87@gmail.com>
2020-11-05 10:03:52 +0100

    Commit

---
+++ 33-introduzione-opp/lib.js
@@ -0,0 +1,38 @@
+/* All'interno di un oggetto, le variabili prendono il nome di "Proprietà" e le funzioni il nome di "Metodi"
+
+I 4 pilastri della Programmagione orientata a gli oggetti sono:
+
+// INCAPSULAMENTO
+-vuol dire racchiudere in una capsula, quindi raggruppare dati e funzioni tra loro correlati.
+Le cui funzioni lavorano internamente con i dati per eseguire delle operazioni
+
+Benefici: Riduzione complessità e incremento riusabilità
+
+// ASTRAZIONE
+Mentre l'incapsulamento è raggruppare dati e funzioni in una capsula e quindi rappresenta il come viene eseguito un compito. Il concetto di astrazione si basa sul cosa. L'obbiettivo dell'astrazione è esporre l'essenziale e nascondere la complessità.
+
+Benefici: Nascondere le complessità e mostrare l'essenziale
+
+
+// EREDITARIETà
+Tramite l'ereditarietà un oggetto può accedere a proprietà e metodi di un altro oggetto evitando duplicazioni e quindi ridondanza del codice
+
+Benefici: Eliminare ridondanza del codice
+
+
+// POLIMORFISMO
+Polimorfismo vuol dire molte forme, nella programnmazione orientata a gli oggetti vuol dire che uno stesso metodo di nome N può avere comportamenti diversi in base al contesto in cui opera.
+
+Per esempio: L'operatore "+" cambia forma in base al contesto in cui viene usato:
+
+- Nei numeri, fa la somma degli elementi.
+- Nelle stringhe, concatena gli elementi.
+
+
+Benefici: Possibilità di far assumere ad una funzione, comportamenti diversi in base al contesto.
+
+*/
+
+
+
+
